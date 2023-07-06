b5d7e32d5d6afbe55f364d9c9d53b1d0fefd543f
patri-create <patriciamkv@gmail.com>
2023-07-06 21:27:58 +0200

    feat: add network handler

--- app/src/main/java/com/project/napptilus/ui/network/NetworkFragment.kt
+++ app/src/main/java/com/project/napptilus/ui/network/NetworkFragment.kt
@@ -1,21 +1,49 @@
 package com.project.napptilus.ui.network
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.core.content.ContextCompat
 import com.project.napptilus.R
+import com.project.napptilus.databinding.FragmentNetworkBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NetworkFragment : Fragment() {
 
+    private lateinit var binding: FragmentNetworkBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_network, container, false)
+    ): View {
+        binding = FragmentNetworkBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        instances()
+    }
+
+    private fun instances() {
+        controlOnBackPress()
+    }
+
+    private fun controlOnBackPress() {
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {}
+            }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
     }
 }
